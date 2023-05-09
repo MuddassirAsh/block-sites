ea5236e8cc3bcb3be4c158e8310da05ed3d6a8f4
MuddassirAsh <muddassirash@gmail.com>
2023-05-08 21:40:43 -0400

    updates

--- editlist/editlist.js
+++ editlist/editlist.js
@@ -3,6 +3,7 @@ const ul = document.getElementById('block-list');
 let rules = [];
 var uniqueID;
 var value;
+var date;
 // outputs domains of the blocked sites from ruleset
 chrome.declarativeNetRequest.getDynamicRules({}, function(rules) {  
     for (let index = 0; index < rules.length; index++) {
@@ -24,23 +25,25 @@ chrome.declarativeNetRequest.getDynamicRules({}, function(rules) {
     }
 })
 
-
 // block a site by adding to the ruleset
-async function blockSite(event){
-
-    console.log("ran blocksite function");
-    await chrome.storage.local.get(["id"]).then((result) => {
-        uniqueID = result.id || 0;  
-        uniqueID++;
-        chrome.storage.local.set({"id": uniqueID})
-        });
-
-    rules.push({"id": uniqueID, "priority": 1, 
-    "action": {"type": "block"}, 
-    "condition": {"urlFilter": document.getElementById('user-input').value, 
-    "resourceTypes": ["main_frame"]}})
-
-    await chrome.declarativeNetRequest.updateDynamicRules({addRules: rules});
+async function blockSite(){
+    if (document.getElementById('user-input').value != ""){
+        await chrome.storage.local.get(["id"]).then((result) => {
+            uniqueID = result.id || 0;  
+            uniqueID++;
+            chrome.storage.local.set({"id": uniqueID})
+            });
+    
+        rules.push({"id": uniqueID, "priority": 1, 
+        "action": {"type": "block"}, 
+        "condition": {"urlFilter": document.getElementById('user-input').value, 
+        "resourceTypes": ["main_frame"]}})
+    
+        await chrome.declarativeNetRequest.updateDynamicRules({addRules: rules});
+    }
+    else{
+        console.error("User input cannot be empty. Please enter a website")
+    }
 
 }
 
@@ -55,3 +58,16 @@ async function unBlockSite (event){
     event.target.parentElement.remove();
     event.target.remove();
 }
+
+function scheduleBlocking(){
+    date = new Date();
+    chrome.alarms.create(
+        "unbBlockingSite",
+        {"delayinMinutes": 2321312321,
+         "periodInMinutes": 23321312321
+        })
+}
+
+// chrome.alarms.onAlarm.addListener(
+//     callback: function,
+//   )
--- todo.md
+++ todo.md
@@ -1,6 +1,7 @@
 - [X] interaction with cached page, remove server worker for pages like Youtube to block domain succesfully
 - [X] remove elements dynamically and editlist.js onclick
 - [ ] add categories/genre that delete a bunch of websites based on selected category like pornography, social media, etc
+- [ ] add scheduling to block sites at certain intervals **working** 
 - [ ] block sites by regex and allowing manually entering domain name **working**
 - [ ] add ability to unblock site with master password 
 - [ ] blocking sites on set intervals and set times e.g. from Monday at 1PM - 5PM
